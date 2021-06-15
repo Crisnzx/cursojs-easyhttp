d150d500f9da0f859c805385a740f3eca24d6d67
Crisnzx <cristovao.neto@academico.ifpb.edu.br>
2021-06-15 16:47:05 -0300

    get post put delete methods

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const http = new EasyHTTP();
 
-http.get('https://ccm-backend.herokuapp.com/usuario', function (error, posts) {
+
+// GET REQUESTS
+
+/*
+http.get('https://ccm-backend.herokuapp.com/usuario', function (error, users) {
    if (error) {
       console.log(error);
    } else {
-      console.log(posts);
+      console.log(users);
    }
-});
+});   */
+
+/*
+http.get('https://ccm-backend.herokuapp.com/usuario/3', function(error, user) {
+   if(!error) {
+      console.log(user);
+
+   } else {
+      console.log(error);
+      
+   }
+});   */
+
 
 const user = {
    nome: 'Savio',
@@ -14,6 +30,25 @@ const user = {
    senha: 'flamengo123'
 }
 
-http.post('https://ccm-backend.herokuapp.com/usuario', user, function(error, response) {
-   console.log(response);
+/* http.post('https://ccm-backend.herokuapp.com/usuario', user, function(error, newUser) {
+   console.log(newUser);
+}) */
+
+const user2 = {
+   id: 5,
+   nome: 'Ximbimbeu',
+   email: 'jurinel@gmail.com',
+   senha: 'cachorrolindo'
+}
+
+/* http.put('https://ccm-backend.herokuapp.com/usuario', user2, function(error, updatedUser) {
+   console.log(updatedUser);
+}); */
+
+http.delete('https://ccm-backend.herokuapp.com/usuario/6', function(error, nothing) {
+   if(!error) {
+      console.log(`${nothing} User deleted`);
+   } else {
+      console.log(error);
+   }
 })
--- easyhttp.js
+++ easyhttp.js
@@ -9,10 +9,8 @@ EasyHTTP.prototype.get = function (url, callback) {
 
    // fix the "this"problem in ES5 without arrow functions
    let self = this;
-   let check = false;
 
    this.http.onload = function () {
-      check = true;
       if (self.http.status === 200) {
          callback(null, self.http.responseText);
 
@@ -20,19 +18,30 @@ EasyHTTP.prototype.get = function (url, callback) {
          callback('Error: ' + self.http.status);
       }
    }
-
-   if (!check) {
-      callback('Error: ' + self.http.status);
-   }
    this.http.send();
 }
 
 // HTTP POST Request
 
-EasyHTTP.prototype.post = function(url, data, callback) {
+EasyHTTP.prototype.post = function (url, data, callback) {
    this.http.open('POST', url, true);
-   this.http.setRequestHeader('Content-Type', 'application/json'); 
+   this.http.setRequestHeader('Content-Type', 'application/json');
 
+   let self = this;
+   this.http.onload = function () {
+      callback(null, self.http.responseText);
+   }
+
+   this.http.send(JSON.stringify(data)); //request body
+}
+
+
+// HTTP PUT Request
+
+EasyHTTP.prototype.put = function(url, data, callback) {
+
+   this.http.open('PUT', url, true);
+   this.http.setRequestHeader('Content-Type', 'application/json');
    let self = this;
    this.http.onload = function() {
       callback(null, self.http.responseText);
@@ -41,7 +50,19 @@ EasyHTTP.prototype.post = function(url, data, callback) {
    this.http.send(JSON.stringify(data));
 }
 
-
-// HTTP PUT Request
-
 // HTTP DELETE Request
+
+EasyHTTP.prototype.delete = function(url, callback) {
+   this.http.open('DELETE', url, true);
+   let self = this;
+
+   this.http.onload = function() {
+      if(self.http.status === 200) {
+         callback(null, self.http.responseText);
+      } else {
+         callback('Error: ' + self.http.status);
+      }
+   }
+
+   this.http.send();
+}
